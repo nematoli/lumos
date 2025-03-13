b158aeeaed4ef9fbd22ffe37e80c13a94bcdf8d7
Iman Nematollahi <nematollahi.iman@gmail.com>
2025-03-13 14:17:20 +0100

    Update download_data.sh

--- dataset/download_data.sh
+++ dataset/download_data.sh
@@ -8,6 +8,11 @@ then
     unzip task_D_D.zip && rm task_D_D.zip
     mv task_A_A task_D_D
     echo "saved folder: task_D_D"
+elif [ "$1" = "lumos_dataset" ]; then
+    echo "Downloading LUMOS dataset ..."
+    wget http://lumos.cs.uni-freiburg.de/datasets/lumos_dataset.zip
+    unzip lumos_dataset.zip && rm lumos_dataset.zip
+    echo "Saved folder: lumos_dataset"
 else
     echo "Failed: Usage download_data.sh calvin | XXX "
     exit 1
