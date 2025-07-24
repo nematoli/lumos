1188256a00bba1c67e206c721d20cc3ecc2738c1
Erfan Azad <erfan.basiri.azad@gmail.com>
2025-07-24 16:02:10 +0200

    replace pyhash with mmh3

--- install.sh
+++ install.sh
@@ -1,2 +1,1 @@
-pip install setuptools==57.5.0
 pip install --no-cache-dir -e .
--- lumos/datasets/base_dataset.py
+++ lumos/datasets/base_dataset.py
@@ -3,9 +3,9 @@ import logging
 from pathlib import Path
 from typing import Dict, Tuple, Union
 
+import mmh3
 import numpy as np
 from omegaconf import DictConfig
-import pyhash
 import torch
 from torch.utils.data import Dataset
 
@@ -18,7 +18,6 @@ from lumos.datasets.utils.episode_utils import (
     process_state,
 )
 
-hasher = pyhash.fnv1_32()
 logger = logging.getLogger(__name__)
 
 
@@ -35,7 +34,9 @@ def get_validation_window_size(idx: int, min_window_size: int, max_window_size: 
         Window size computed with hash function.
     """
     window_range = max_window_size - min_window_size + 1
-    return min_window_size + hasher(str(idx)) % window_range
+
+    hash_val = mmh3.hash(str(idx)) & 0xFFFFFFFF  # ensure unsigned 32-bit
+    return min_window_size + (hash_val % window_range)
 
 
 class BaseDataset(Dataset):
