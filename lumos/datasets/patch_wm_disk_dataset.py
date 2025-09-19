206f847bc4c043352eb9f9a662e90222eeee85c1
acl21 <14245491+acl21@users.noreply.github.com>
2025-09-19 17:53:09 +0200

    fixing the features key issue in code

--- lumos/datasets/patch_wm_disk_dataset.py
+++ lumos/datasets/patch_wm_disk_dataset.py
@@ -1,6 +1,7 @@
+from itertools import chain
 import logging
 import pickle
-from typing import Any
+from typing import Any, Dict
 
 import numpy as np
 from tqdm import tqdm
@@ -44,6 +45,51 @@ class PatchWMDiskDataset(BaseWMDiskDataset):
         if self.use_cached_data:
             self.preloaded_data = {}  # Initialize as a dictionary
             self.preload_dataset(self.abs_datasets_dir / "cached_data.pkl")
+
+        self.keys = list(chain(*self.observation_space.values()))
+        self.key_map = {"patches": "features"}
+        self.key_map.update({key: key if key not in self.key_map else self.key_map[key] for key in self.keys})
+
+    def _load_episode(self, idx: int, window_size: int) -> Dict[str, np.ndarray]:
+        """
+        Load consecutive frames saved as individual files on disk and combine to episode dict.
+
+        Args:
+            idx: Index of first frame.
+            window_size: Length of sampled episode.
+
+        Returns:
+            episode: Dict of numpy arrays containing the episode where keys are the names of modalities.
+        """
+        start_idx = self.episode_lookup[idx]
+        end_idx = start_idx + window_size
+
+        resets = np.zeros((window_size, 1), dtype=bool)
+        if self.reset_prob != 0:
+            resets = np.random.rand(window_size, 1) <= self.reset_prob
+        zero_action = np.zeros(7, dtype=np.float64)
+        zero_action[-1] = 1.0
+
+        if start_idx in self.start_ids:
+            episodes = self.zip_sequence(start_idx, end_idx)
+            episode = {key: np.stack([ep[self.key_map[key]] for ep in episodes]) for key in self.keys}
+
+            episode["pre_actions"] = np.roll(episode["rel_actions"], shift=1, axis=0)
+            episode["pre_actions"][0] = zero_action
+
+            episode["pre_robot_obs"] = np.roll(episode["robot_obs"], shift=1, axis=0)
+            resets[0] = True
+        else:
+            episodes = self.zip_sequence(start_idx - 1, end_idx)
+            episode = {key: np.stack([ep[self.key_map[key]] for ep in episodes[1:]]) for key in self.keys}
+
+            episode["pre_actions"] = np.stack([ep["rel_actions"] for ep in episodes[:-1]])
+
+            episode["pre_robot_obs"] = np.stack([ep["robot_obs"] for ep in episodes[:-1]])
+
+        episode["reset"] = resets
+        episode["frame"] = np.arange(start_idx, end_idx, dtype=np.int32)[:, np.newaxis]
+        return episode
 
     def preload_dataset(self, cached_data_path):
         """Preloads the entire dataset into memory."""
