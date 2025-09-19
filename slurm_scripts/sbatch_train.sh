07939c8741ca31a5d9d9c9911ab52b9bca9d170f
acl21 <14245491+acl21@users.noreply.github.com>
2025-09-19 18:18:07 +0200

    added slurm scripts
    Signed-off-by: acl21 <14245491+acl21@users.noreply.github.com>

--- slurm_scripts/sbatch_train.sh
+++ slurm_scripts/sbatch_train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name append-calvin-data
+#SBATCH --job-name train-patch-wm
 #SBATCH --requeue
 ##SBATCH --exclude=dlc2gpu10,dlc2gpu07
 #SBATCH --signal=SIGUSR1@600
@@ -10,13 +10,11 @@
 #SBATCH --cpus-per-task=32
 #SBATCH --mem=320G
 #SBATCH --time=1-00:00
-##SBATCH --partition rldlc2_gpu-l40s
+#SBATCH --partition rldlc2_gpu-l40s
 ##SBATCH --partition alldlc2_gpu-l40s
 ##SBATCH --partition alldlc2_gpu-h200
-#SBATCH --partition alldlc_gpu-rtx2080
+##SBATCH --partition alldlc_gpu-rtx2080
 ##SBATCH --partition aisdlc_gpu-rtx2080
-
-
 
 #SBATCH --output=/work/dlclarge1/lagandua-vlawm/lumos/logs/outputs/%x_%j.out
 #SBATCH --error=/work/dlclarge1/lagandua-vlawm/lumos/logs/outputs/%x_%j.err
@@ -46,17 +44,24 @@ ulimit -n ${file_limit}
 echo "Set soft file limit to ${file_limit}"
 
 # prepare and run
-export NCCL_P2P_DISABLE=1
+export NCCL_DEBUG=INFO
 export PYTHONFAULTHANDLER=1
-export TORCH_USE_CUDA_DSA=1
+export HYDRA_FULL_ERROR=1
 
-echo ""
-echo "== LOG ================================================================================================================="
-
-# Activate the virtualenv / conda environment
+# Job to perform
 source /home/lagandua/.bashrc
-conda activate lumos
+conda activate $1
 
+GPUS=$4
+timeout 23.8h srun  --gres=gpu:${GPUS} python $2 slurm=true hydra.run.dir=$3 trainer.devices=$GPUS ${@:5}
 
-srun python /work/dlclarge1/lagandua-vlawm/lumos/scripts/append_calvin_patch_dataset.py
-# batch_size=2 model=smolflow_agent # batch_size=4 model.use_lora=False # model=vlm_berg_agent # batch_size=2 model.vla_mode='reduced_head' #model.use_perceiver=False model.use_incontext=True #seed=242 #model=mode_agent
+if [[ $? -eq 124 ]]; then
+echo "Time limit exceeded. Resubmit job.";
+ssh ${USER}@kis3bat2 <<ENDSSH
+sh $3/resume_training.sh
+ENDSSH
+fi
+
+# Print some Information about the end-time to STDOUT
+echo "DONE";
+echo "Finished at $(date)";
